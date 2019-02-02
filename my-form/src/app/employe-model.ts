db05ed5dcb651eb389e0e8be0ee013b4291364f0
Rahul Mondal <rahul.arambagh96@gmail.com>
2019-02-02 10:15:01 +0530

    Files are uploaded

---
+++ my-form/src/app/employe-model.ts
@@ -0,0 +1,17 @@
+export class EmployeModel {
+    constructor(
+        public FirstName: string,
+        public LastName: string,
+        public Email:string,
+        public Contact:number,
+        public Address:string,
+        public Username:string,
+        public Password:string,
+        public Gender:string,
+        public Qualification:string,
+        public Experience: string,
+        public Language:string[],
+    ){}
+    
+}
+
